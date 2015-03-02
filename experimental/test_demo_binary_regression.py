9350da0292f6b59543d467ca74b8a3b71f50d859
Peter <peter.ed.oconnor@gmail.com>
2015-03-02 18:33:34 +0100

    still need to clean up ensemble thing

--- experimental/test_demo_binary_regression.py
+++ experimental/test_demo_binary_regression.py
@@ -6,7 +6,7 @@ __author__ = 'peter'
 def test_all_create_all_figures():
 
     for figure in ['1', '2A', '2B', '2C', '2D', '3A', '3B', '4A', '4B', '4C', '4D', '5A', '5B',
-            '5C', '5D', '6A', '6B']:
+            '5C', '5D', '6A', '6B', 'crohns']:
         demo_create_figure(figure, test_mode = True)
 
 
