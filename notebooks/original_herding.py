efe02b64ffa14db70758fadc190f51193cdbc853
Peter <peter.ed.oconnor@gmail.com>
2015-03-03 22:32:25 +0100

    works now

--- notebooks/original_herding.py
+++ notebooks/original_herding.py
@@ -9,7 +9,7 @@ from matplotlib import pyplot as plt
 # <codecell>
 
 # Setup functions
-binary_count_matrix = lambda n: ((np.arange(2**n)[:, None] >> np.arange(n-1, -1, -1)[None, :]).astype(bool) & True)
+binary_count_matrix = lambda n_bits: np.right_shift(np.arange(2**n_bits)[:, None], np.arange(n_bits-1, -1, -1)[None, :]) & 1
 
 sigm = lambda x: 1/(1+np.exp(-x))
 
@@ -28,10 +28,10 @@ def random_symmetric_mat(mag, power, n_dims, rng):
 
 # <codecell>
 
-mag = 0.4      # la
-w_power = 1.   # a
-n_steps = 50000  # T
-n_dims = 18   # N
+mag = 0.4        # a.k.a. la
+w_power = 1.     # a.k.a. a
+n_steps = 500000 # a.k.a. T
+n_dims = 12      # a.k.a. N
 seed = None
 
 # <codecell>
@@ -39,6 +39,8 @@ seed = None
 rng = np.random.RandomState(seed)
 biases = mag*np.random.randn(n_dims)
 weights = random_symmetric_mat(mag = mag, power = w_power, n_dims = n_dims, rng = rng)
+assert np.all(weights[np.arange(n_dims), np.arange(n_dims)]==0)
+assert np.array_equal(weights, weights.T)
 
 # <codecell>
 
@@ -52,9 +54,9 @@ def exact_marginals_func(weights, biases):
     assert n_nodes == len(biases) == weights.shape[1]
     assert np.allclose(weights, weights.T)
     bmat = binary_count_matrix(n_nodes).astype(float)  # (2**n_dims, n_dims)
-    inner_sum = 0.5*np.einsum('ij,jk,ik->i', bmat, weights, bmat) + (bmat*biases).sum(axis=1)  # (2**n_dims, )
+    energy = 0.5*np.einsum('ij,jk,ik->i', bmat, weights, bmat) + (bmat*biases).sum(axis=1)  # (2**n_dims, )
     
-    exp_log_prob = np.exp(inner_sum)  # (2**n_dims, )
+    exp_log_prob = np.exp(energy)  # (2**n_dims, )  # Negative sign?
     q = np.sum(exp_log_prob[:, None]*bmat, axis = 0)  # (n_dims, )
     normalizer = np.sum(exp_log_prob)  # Scalar
     marginals = q/normalizer
@@ -63,7 +65,8 @@ exact_marginals = exact_marginals_func(weights, biases)
 
 # <codecell>
 
-def gibbs_fc(weights, biases, n_steps, rng, block = False):
+# Get the Gibbs Stats
+def gibbs_sample(weights, biases, n_steps, rng, block = False):
     """
     weights is a (n_nodes, n_nodes) symmetric matrix
     biases is a (n_nodes, ) vector
@@ -72,7 +75,6 @@ def gibbs_fc(weights, biases, n_steps, rng, block = False):
     
     returns records: A (n_steps, n_nodes) array indicating the state of the MCMC at each step. 
     """
-    
     n_dims = weights.shape[0]
     n_samples = len(biases)
     assert n_dims == len(biases) == weights.shape[1]
@@ -83,24 +85,54 @@ def gibbs_fc(weights, biases, n_steps, rng, block = False):
             x = sigm(x.dot(weights)+biases) > rng.rand(n_dims)
         else:
             for i in xrange(n_dims):
-                x[i] = sigm(x.dot(weights[i])+biases[i]) > rng.rand() 
+                x[i] = sigm(x.dot(weights[i, :])+biases[i]) > rng.rand()
         records[t, :] = x
     return records  
+single_gibbs_records = gibbs_sample(weights, biases, n_steps, rng=rng, block = False)
+block_gibbs_records = gibbs_sample(weights, biases, n_steps, rng=rng, block = True)
 
-single_gibbs_records = gibbs_fc(weights, biases, n_steps, rng=rng, block = False)
-block_gibbs_records = gibbs_fc(weights, biases, n_steps, rng=rng, block = True)
+# <codecell>
+
+# Get the Herding Stats
+def herded_sample(weights, biases, n_steps, rng, block = False):
+    """
+    weights is a (n_nodes, n_nodes) symmetric matrix
+    biases is a (n_nodes, ) vector
+    n_steps is the number of steps to run Gibbs sampling for 
+    rng is a random number generator
+    
+    returns records: A (n_steps, n_nodes) array indicating the state of the MCMC at each step. 
+    """
+    n_dims = weights.shape[0]
+    n_samples = len(biases)
+    assert n_dims == len(biases) == weights.shape[1]
+    records = np.empty((n_steps, n_dims))
+    x = 0.5 > rng.rand(n_dims)
+    h = np.zeros(x.shape)
+    for t in xrange(n_steps):
+        if block:
+            p = sigm(x.dot(weights)+biases)
+            h += p
+            x = h > 0.5
+            h -= x
+        else:
+            for i in xrange(n_dims):
+                p = sigm(x.dot(weights[i, :])+biases[i])
+                h[i] += p
+                x[i] = h[i] > 0.5
+                h[i] -= x[i]
+        records[t, :] = x
+    return records  
+single_herded_records = herded_sample(weights, biases, n_steps, rng=rng, block = False)
+block_herded_records = herded_sample(weights, biases, n_steps, rng=rng, block = True)
 
 # <codecell>
 
 # Compute Error curves
-TARGET = 'gibbs-final'
-target_distribution = {
-    'gibbs-final': np.mean(single_gibbs_records, axis = 0),
-    'exact': exact_marginals
-    }[TARGET]
-
-single_gibbs_error = L1_error(cummean(single_gibbs_records, axis = 0), target_distribution)
-block_gibbs_error = L1_error(cummean(block_gibbs_records, axis = 0), target_distribution)
+single_gibbs_error = L1_error(cummean(single_gibbs_records, axis = 0), exact_marginals)
+block_gibbs_error = L1_error(cummean(block_gibbs_records, axis = 0), exact_marginals)
+single_herded_error = L1_error(cummean(single_herded_records, axis = 0), exact_marginals)
+block_herded_error = L1_error(cummean(block_herded_records, axis = 0), exact_marginals)
 
 # <codecell>
 
@@ -108,6 +140,11 @@ block_gibbs_error = L1_error(cummean(block_gibbs_records, axis = 0), target_dist
 plt.figure()
 plt.loglog(single_gibbs_error)
 plt.loglog(block_gibbs_error)
+plt.loglog(single_herded_error)
+plt.loglog(block_herded_error)
+plt.loglog([1, n_steps], [.5, n_steps**-1])
+plt.loglog([1, n_steps], [.5, n_steps**-.5])
+plt.legend(['Gibbs', 'Block-Gibbs', 'Herding', 'Block-Herding', '1/x', '1/sqrt(x)'], loc='best')
 plt.show()
 
 # <codecell>
