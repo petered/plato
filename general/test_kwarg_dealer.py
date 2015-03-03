a2dc4cba5cf322628d5b26004d8c7749241f7969
Peter <peter.ed.oconnor@gmail.com>
2015-03-03 13:29:36 +0100

    fixed_test

--- general/test_kwarg_dealer.py
+++ general/test_kwarg_dealer.py
@@ -1,32 +1,37 @@
 from general.kwarg_dealer import KwargDealer
+import pytest
 
 __author__ = 'peter'
 
 
 def my_favourite_system(planet, **kwargs):
-    kd = KwargDealer(**kwargs)
+    kd = KwargDealer(kwargs)
 
     if planet == 'Earth':
-        params = kd({'moon': 'the_moon', 'person': 'Jesus'})
-        favsys = 'My Faviourite planet is %s, my favourite moon is %s, and my faviourite person is %s' %(planet, params['moon'], params['person'])
+        params = kd.deal({'moon': 'The Moon', 'person': 'Jesus'})
+        favsys = 'My favourite planet is %s, my favourite moon is %s, and my favourite person is %s' %(planet, params['moon'], params['person'])
     elif planet == 'Mars':
-        params = kd({'moon': 'Phobos'})
-        favsys = 'My Favourite planet is %s, my favourite moon is %s' % (planet, params['moon'])
+        params = kd.deal({'moon': 'Phobos'})
+        favsys = 'My favourite planet is %s, my favourite moon is %s' % (planet, params['moon'])
     elif planet == 'Venus':
-        favsys == 'My favourite planet is Venus.'
+        favsys = 'My favourite planet is %s.' % (planet, )
 
-    assert kd.is_empty(), 'Keyw'
-    print favsys
+    kd.assert_empty()
+    return favsys
 
 
 def test_kwarg_dealer():
 
+    assert my_favourite_system('Earth') == 'My favourite planet is Earth, my favourite moon is The Moon, and my favourite person is Jesus'
+    assert my_favourite_system('Earth', person = 'Margerit Thatcher') == 'My favourite planet is Earth, my favourite moon is The Moon, and my favourite person is Margerit Thatcher'
+    assert my_favourite_system('Mars') == 'My favourite planet is Mars, my favourite moon is Phobos'
+    assert my_favourite_system('Venus') == 'My favourite planet is Venus.'
 
+    with pytest.raises(AssertionError):
+        my_favourite_system('Mars', person = 'Jesus')  # No people on Mars
 
-    for figure in figure_numbers:
-        print 'Testing Figure %s ...' % (figure, )
-        demo_create_figure(figure, test_mode = True)
-        print '... Passed.'
+    with pytest.raises(AssertionError):
+        my_favourite_system('Venus', moon = 'Europa')  # No moons around Venus
 
 if __name__ == '__main__':
     test_kwarg_dealer()
