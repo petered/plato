22b04e805ef87aa0cebceb597f97ee6880164dd3
Peter <peter.ed.oconnor@gmail.com>
2015-03-06 18:04:12 +0100

    fdsfds

--- notebooks/config/strip_notebook_output.py
+++ notebooks/config/strip_notebook_output.py
@@ -27,10 +27,11 @@ def strip_output(nb):
 
 if __name__ == '__main__':
     filename = sys.argv[1]
-    with io.open(filename, 'r', encoding='utf8') as f:
-        nb = nbformat.read(f, as_version=4)
-        #nb = current.read(f, 'json')
-    nb = strip_output(nb)
-    with io.open(filename, 'w', encoding='utf8') as f:
-        nbformat.write(nb, f)
-        #current.write(nb, f, 'json')
+    if filename.endswith('.ipynb'):
+        with io.open(filename, 'r', encoding='utf8') as f:
+            nb = nbformat.read(f, as_version=4)
+            #nb = current.read(f, 'json')
+        nb = strip_output(nb)
+        with io.open(filename, 'w', encoding='utf8') as f:
+            nbformat.write(nb, f)
+            #current.write(nb, f, 'json')
