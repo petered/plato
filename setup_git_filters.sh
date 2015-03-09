c9ca2e7f036e69052ca267df44d28c3750a5081c
Peter <peter.ed.oconnor@gmail.com>
2015-03-09 15:01:43 +0100

    ok?

--- setup_git_filters.sh
+++ setup_git_filters.sh
@@ -1,11 +1,8 @@
 
-
-# Add a filter named strip output for all .ipynb files
-printf "*.ipynb filter=stripoutput\n" >> ./.gitattributes
-
 # Link this filter to the file that does the filtering
-# printf '[filter "stripoutput"]\n\tclean = %s/notebooks/config/strip_notebook_output\n\trequired\n' $(git rev-parse --show-toplevel) >> ./.git/config
-
 git config filter.stripoutput.clean "$(git rev-parse --show-toplevel)/notebooks/config/strip_notebook_output" 
 git config filter.stripoutput.smudge cat
 git config filter.stripoutput.required true
+
+# Which is better than:
+# printf '[filter "stripoutput"]\n\tclean = %s/notebooks/config/strip_notebook_output\n\trequired\n' $(git rev-parse --show-toplevel) >> ./.git/config
