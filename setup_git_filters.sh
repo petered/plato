6f69cbb24b2675e04552ce640270ceca501f5b87
Peter <peter.ed.oconnor@gmail.com>
2015-03-09 14:58:40 +0100

    fdsfds

--- setup_git_filters.sh
+++ setup_git_filters.sh
@@ -4,4 +4,8 @@
 printf "*.ipynb filter=stripoutput\n" >> ./.gitattributes
 
 # Link this filter to the file that does the filtering
-printf '[filter "stripoutput"]\n\tclean = %s/notebooks/config/strip_notebook_output\n\trequired\n' $(git rev-parse --show-toplevel) >> ./.git/config
+# printf '[filter "stripoutput"]\n\tclean = %s/notebooks/config/strip_notebook_output\n\trequired\n' $(git rev-parse --show-toplevel) >> ./.git/config
+
+git config filter.stripoutput.clean "$(git rev-parse --show-toplevel)/notebooks/config/strip_notebook_output" 
+git config filter.stripoutput.smudge cat
+git config filter.stripoutput.required true
