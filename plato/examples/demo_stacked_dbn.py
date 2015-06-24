4dc095168e804ffc278183c0d0469288a092a817
Peter <peter.ed.oconnor@gmail.com>
2015-06-24 13:41:56 +0200

    changed size thing

--- plato/examples/demo_stacked_dbn.py
+++ plato/examples/demo_stacked_dbn.py
@@ -10,12 +10,14 @@ from utils.tools.stacked_dbn import StackedDeepBeliefNet, BernoulliBernoulliRBM,
 def demo_simple_dbn(
         minibatch_size = 10,
         n_training_epochs_1 = 5,
-        n_training_epochs_2 = 5,
+        n_training_epochs_2 = 10,
         n_hidden_1 = 500,
         n_hidden_2 = 10,
         plot_period = 100,
         eta1 = 0.01,
-        eta2 = 0.001,
+        eta2 = 0.00001,
+        w_init_mag_1 = 0.01,
+        w_init_mag_2 = 0.5,
         seed = None
         ):
     """
@@ -33,14 +35,15 @@ def demo_simple_dbn(
 
     dataset = get_mnist_dataset(flat = True)
     rng = np.random.RandomState(seed)
-    w_init = lambda shape: 0.01 * rng.randn(*shape)
+    w_init_1 = lambda shape: w_init_mag_1 * rng.randn(*shape)
+    w_init_2 = lambda shape: w_init_mag_2 * rng.randn(*shape)
 
     if is_test_mode():
         n_training_epochs_1 = 0.01
         n_training_epochs_2 = 0.01
 
     # Train the first RBM
-    dbn1 = StackedDeepBeliefNet(rbms = [BernoulliBernoulliRBM.from_initializer(n_visible = 784, n_hidden=n_hidden_1, w_init_fcn = w_init)])
+    dbn1 = StackedDeepBeliefNet(rbms = [BernoulliBernoulliRBM.from_initializer(n_visible = 784, n_hidden=n_hidden_1, w_init_fcn = w_init_1)])
     train_first_layer = dbn1.get_training_fcn(optimizer=SimpleGradientDescent(eta = eta1), n_gibbs = 1, persistent=True).compile()
     sample_first_layer = dbn1.get_sampling_fcn(initial_vis=dataset.training_set.input[:minibatch_size], n_steps = 10).compile()
     for i, vis_data in enumerate(minibatch_iterate(dataset.training_set.input, minibatch_size=minibatch_size, n_epochs=n_training_epochs_1)):
@@ -50,7 +53,7 @@ def demo_simple_dbn(
         train_first_layer(vis_data)
 
     # Train the second RBM
-    dbn2 = dbn1.stack_another(rbm = BernoulliGaussianRBM.from_initializer(n_visible=n_hidden_1, n_hidden=n_hidden_2, w_init_fcn=w_init))
+    dbn2 = dbn1.stack_another(rbm = BernoulliGaussianRBM.from_initializer(n_visible=n_hidden_1, n_hidden=n_hidden_2, w_init_fcn=w_init_2))
     train_second_layer = dbn2.get_training_fcn(optimizer=SimpleGradientDescent(eta = eta2), n_gibbs = 1, persistent=True).compile()
     sample_second_layer = dbn2.get_sampling_fcn(initial_vis=dataset.training_set.input[:minibatch_size], n_steps = 10).compile()
     for i, vis_data in enumerate(minibatch_iterate(dataset.training_set.input, minibatch_size=minibatch_size, n_epochs=n_training_epochs_2)):
