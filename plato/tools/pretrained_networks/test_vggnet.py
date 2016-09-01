adc91748d69b88b7830b3c7973d89402ee7bc191
Peter O'Connor <peter.ed.oconnor@gmail.com>
2016-09-01 17:39:52 +0200

    removed other vggtest for unused norm thing

--- plato/tools/pretrained_networks/test_vggnet.py
+++ plato/tools/pretrained_networks/test_vggnet.py
@@ -16,21 +16,6 @@ def test_vggnet():
     assert label == 'American egret, great white heron, Egretta albus'
 
 
-@pytest.mark.slowtest
-def test_norm_vggnet():
-
-    vggnet = get_vgg_net(normalized=True)
-    f_predict = vggnet.compile(add_test_values = False)
-    inp = im2vgginput(get_image('heron'))
-    out = f_predict(inp)
-    label = get_vggnet_labels()[np.argmax(out)]
-    print label
-    assert label == 'little blue heron, Egretta caerulea'  # Oddly not the same as regular vggnet...
-
-
-
-
 if __name__ == '__main__':
 
     test_vggnet()
-    test_norm_vggnet()
