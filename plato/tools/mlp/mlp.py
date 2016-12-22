bbae4d9fe6684d60392da5bc95821a6d167a97ed
peter <peter.ed.oconnor@gmail.com>
2016-12-22 16:34:52 +0100

    sumthin

--- plato/tools/mlp/mlp.py
+++ plato/tools/mlp/mlp.py
@@ -1,5 +1,7 @@
+from theano.compile.sharedvalue import SharedVariable
+
 from artemis.general.numpy_helpers import get_rng
-from artemis.general.should_be_builtins import bad_value
+from artemis.general.should_be_builtins import bad_value, izip_equal
 from artemis.ml.tools.neuralnets import initialize_network_params
 from plato.interfaces.helpers import get_named_activation_function, batch_normalize
 from plato.core import create_shared_variable, symbolic_simple, symbolic
@@ -16,15 +18,7 @@ class MultiLayerPerceptron(IParameterized):
 
     def __init__(self, layers):
         """
-        :param weights: A list of initial weight matrices of shapes:
-            [(n_in, n_hidden_0), (n_hidden_0, n_hidden_1), ... (n_hidden_N, n_out)]
-        :param hidden_activation: A string or list of strings indicating the type of each hidden layer.
-            {'sig', 'tanh', 'rect-lin', 'lin', 'softmax'}
-        :param output_activation: A string (see above) identifying the activation function for the output layer
-        :param normalize_minibatch: True to normalize by mean and standard-deviation over the minibatch.
-        :param scale_param: Add a parameter in addition to the biases for rescaling before the nonlinearity.  Only
-            really makes sense when normalize_minibatch is True.
-        :param use_bias: If False, do not use biases.
+        :param layers: A list of Layer object (see below)
         """
         self.layers = layers
 
@@ -46,50 +40,59 @@ class MultiLayerPerceptron(IParameterized):
         return [param for layer in self.layers for param in layer.parameters]
 
     @classmethod
-    def from_init(cls, w_init, layer_sizes, w_init_dist='normal', rng=None, last_layer_zero=False, **init_args):
+    def from_init(cls, w_init, layer_sizes, w_init_dist='normal', rng=None, last_layer_zero=False, use_bias=True, **init_args):
         """
         :param w_init: Can be:
             - A scalar, in which case w_init will be interpreted as the standard deviation for the Normally distributed initial weights.
             - A function which accepts the shape of the weight matrix as separate arguments.
         :param layer_sizes: A list of layer sizes, including the input layer
+        :param w_init_dist: The distribution of initial weights
         :param rng: A random number generator or seed to use for drawing weights (only when w_init is a scalar)
         :param last_layer_zero: There is no need for the last layer to have initial weights.  If this is True, the weights of
             the last layer will all be zero.
-        :param **init_args: See MultiLayerPerceptron constructor
+        :param **init_args: Passed down to MultiLayerPerceptron.from_weights constructor below
         """
         assert len(layer_sizes)>1, "A Multi-layer perceptron with only 1 layer don't make no sense.  Remember, input/output layers must be specified."
         if hasattr(w_init, '__call__'):
             assert rng is None, "If w_init is callable, the random number generator (rng) doesn't do anything, and shouldn't be specified."
         else:
             rng = get_rng(rng)
-            w_init_mag = w_init
-        #     w_init = lambda n_inputs, n_outputs: w_init_mag * rng.randn(n_in, n_out)
-        # weights = [w_init(n_in, n_out) for n_in, n_out in zip(layer_sizes[:-1], layer_sizes[1:])]
-
             weights = initialize_network_params(layer_sizes=layer_sizes, mag=w_init, base_dist=w_init_dist, include_biases=False, rng=rng)
-
         if last_layer_zero:
             weights[-1][:] = 0
-        return cls.from_weights(weights=weights, **init_args)
+        return cls.from_weights(weights=weights, biases=None if use_bias else False, **init_args)
 
     @classmethod
-    def from_weights(cls, weights, hidden_activation = 'sig', output_activation = 'sig',
-            normalize_minibatch = False, scale_param = False, use_bias = True):
-        assert all(w_in.shape[-1] == w_out.shape[-2] for w_in, w_out in zip(weights[:-1], weights[1:]))
+    def from_weights(cls, weights, biases = None, hidden_activation = 'sig', output_activation = 'sig',
+            normalize_minibatch = False, scale_param = False):
+        """
+        :param weights: A list of weight matrices with shapes: [(n_in, n_hid1), (n_hid1, n_hid2), ..., (n_hid_last, n_out)]
+        :param biases: Either (1) A list of biases with shapes [(n_hid1, ), ... (n_out, )], (2) None, indicating zero
+            biases, or (3) False, indicating "don't use biases"
+        :param hidden_activation: The hidden activation function
+        :param output_activation: The output activation function
+        :param normalize_minibatch: True to normalize by mean and standard-deviation over the minibatch.
+        :param scale_param: Add a parameter in addition to the biases for rescaling before the nonlinearity.  Only
+            really makes sense when normalize_minibatch is True.
+        :return: A MLP
+        """
+        w_shapes = [w.shape if isinstance(w, np.ndarray) else w.get_value().shape for w in weights]
+        assert all(w_in_shape[-1] == w_out_shape[-2] for w_in_shape, w_out_shape in zip(w_shapes[:-1], w_shapes[1:]))
         n_layers = len(weights)
         layers = [
             Layer(
                 linear_transform=FullyConnectedTransform(
                     w=w,
+                    b = b,
                     normalize_minibatch=normalize_minibatch if layer_no < (n_layers - 1) else None,
                     scale=scale_param,
-                    use_bias=use_bias
+                    use_bias=biases is not False
                 ),
                 nonlinearity=nonlinearity
             )
-            for w, nonlinearity, layer_no in
-            zip(weights, [hidden_activation] * (n_layers - 1) + [output_activation], xrange(n_layers))
-            ]
+            for w, b, nonlinearity, layer_no in
+                izip_equal(weights, [0.]*len(weights) if biases is None else biases, [hidden_activation] * (n_layers - 1) + [output_activation], xrange(n_layers))
+                ]
         return MultiLayerPerceptron(layers)
 
 
@@ -143,8 +146,8 @@ class FullyConnectedTransform(IParameterized):
             normalize_minibatch is True.
         :param use_bias: Use a bias term?  Generally, the answer is "True", a bias term helps.
         """
-        self.w = create_shared_variable(w, name = 'w')
-        self.b = create_shared_variable(b, shape = w.shape[1] if w.ndim==2 else (w.shape[0], w.shape[2]) if w.ndim==3 else bad_value(w.shape), name = 'b')
+        self.w = w if isinstance(w, SharedVariable) else create_shared_variable(w, name = 'w')
+        self.b = b if isinstance(b, SharedVariable) else create_shared_variable(b, shape = w.shape[1] if w.ndim==2 else (w.shape[0], w.shape[2]) if w.ndim==3 else bad_value(w.shape), name = 'b')
         self.log_scale = create_shared_variable(0 if scale else None, shape = w.shape[1], name = 'log_scale') if scale else None
         self.normalizer = \
             batch_normalize if normalize_minibatch is True else \
