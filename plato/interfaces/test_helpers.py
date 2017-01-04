5a7c3276b328560355b919fae72a709c6c07707a
Peter O'Connor <peter.ed.oconnor@gmail.com>
2017-01-04 17:25:08 +0100

    added batchify

--- plato/interfaces/test_helpers.py
+++ plato/interfaces/test_helpers.py
@@ -1,5 +1,6 @@
+from plato.core import symbolic, create_shared_variable, add_update
 from plato.interfaces.decorators import symbolic_simple
-from plato.interfaces.helpers import MRG_RandomStreams_ext
+from plato.interfaces.helpers import MRG_RandomStreams_ext, batchify_function
 import numpy as np
 import pytest
 
@@ -29,6 +30,33 @@ def test_mrg_choice():
     assert all(ixs2 < 10) and len(np.unique(ixs2)) == len(ixs2)
 
 
+def test_compute_in_batches():
+
+    @symbolic
+    def add_them(a, b):
+        return a+b
+    arr_a = np.random.randn(60, 3)  # (note that say (57, 3) would fail
+    arr_b = np.random.randn(60, 3)
+    f = batchify_function(add_them, batch_size=10)
+    out = f.compile()(arr_a, arr_b)
+    assert np.allclose(out, arr_a+arr_b)
+
+
+def test_compute_in_with_state():
+
+    @symbolic
+    def add_them(a):
+        i = create_shared_variable(0.)
+        add_update(i, i+1)
+        return a+i
+
+    arr = np.random.randn(60, 3)
+    out = batchify_function(add_them, 10).compile()(arr)
+    assert all(np.allclose(out[i*10:(i+1)*10], arr[i*10:(i+1)*10]+i) for i in xrange(6))
+
+
 if __name__ == '__main__':
 
     test_mrg_choice()
+    test_compute_in_batches()
+    test_compute_in_with_state()
